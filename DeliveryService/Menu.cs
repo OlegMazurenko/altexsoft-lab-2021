9e91eae360099a810b6b33e63c8d3ba00f64ccc3
Олег Мазуренко <Mazyr7@outlook.com>
2021-04-22 10:16:12 +0300

    Created Menu

---
+++ DeliveryService/Menu.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeliveryService.Controllers;
+using DeliveryService.Models;
+
+namespace DeliveryService
+{
+    public class Menu
+    {
+        public void ShowMenu()
+        {
+            var productController = new ProductController();
+            bool end = false;
+            while (!end)
+            {
+                Console.WriteLine("\r\nDeliveryService");
+                Console.WriteLine("Введите 1, чтобы войти как покупатель");
+                Console.WriteLine("Введите 2, чтобы войти как продавец");
+                Console.WriteLine("Введите 0, чтобы выйти");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    switch (Convert.ToInt32(input))
+                    {
+                        case 1:
+                            MakeOrder(productController);
+                            break;
+                        case 2:
+                            CreateProduct(productController);
+                            break;
+                        case 0:
+                            end = true;
+                            break;
+                        default:
+                            Console.WriteLine("Введен неправильный номер");
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Введены некорректные данные");
+                }
+            }
+        }
+
+        public void MakeOrder(ProductController productController)
+        {
+            bool goToOrder = false;
+            var productsToOrder = new List<Product>();
+            while (!goToOrder)
+            {
+                Console.WriteLine("Список товаров:");
+                var products = productController.GetProducts();
+                Console.WriteLine("Имя | Описание | Цена | Продавец");
+                foreach (var product in products)
+                {
+                    Console.WriteLine($"{product.Name} | {product.Description} | {product.Price} | {product.Seller}");
+                }
+                var productsToOrderCount = 0;
+                while (productsToOrderCount < 1)
+                {
+                    Console.WriteLine("Введите имя товара, который хотите заказать");
+                    var inputProductName = Console.ReadLine();
+                    foreach (var product1 in products)
+                    {
+                        if (product1.Name == inputProductName)
+                        {
+                            productsToOrder.Add(product1);
+                            productsToOrderCount += 1;
+                        }
+                    }
+                    if (productsToOrderCount < 1)
+                    {
+                        Console.WriteLine("Не удалось найти товар с таким именем, попробуйте еще раз");
+                    }
+                }
+                Console.WriteLine("Введите 1, чтобы перейти к оформлению заказа");
+                Console.WriteLine("Введите любое другое число, чтобы добавить в заказ другие товары");
+                var input1 = Convert.ToInt32(Console.ReadLine());
+                if (input1 == 1)
+                {
+                    goToOrder = true;
+                }
+            }
+            Console.WriteLine("Введите адресс доставки:");
+            var inputAdress = Console.ReadLine();
+            var order = new Order(inputAdress, productsToOrder);
+            Console.WriteLine("Заказ успешно сделан!");
+        }
+
+        public void CreateProduct(ProductController productController)
+        {
+            Console.WriteLine("Добавление нового продукта");
+            Console.WriteLine("Введите название продукта:");
+            var productName = Console.ReadLine();
+            Console.WriteLine("Введите описание продукта:");
+            var productDescription = Console.ReadLine();
+            Console.WriteLine("Введите цену продукта:");
+            var productPrice = Convert.ToDecimal(Console.ReadLine());
+            var newProduct = new Product(productName, productDescription, productPrice, "SellerName");
+            productController.CreateProduct(newProduct);
+            Console.WriteLine("Товар успешно добавлен!");
+        }
+    }
+}
