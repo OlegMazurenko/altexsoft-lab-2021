0c4442c73135225bde5bb8f77ac0485dd34cc70a
Олег Мазуренко <Mazyr7@outlook.com>
2021-06-04 06:14:05 +0300

    updateTime fix

--- DeliveryService/Data/Cache.cs
+++ DeliveryService/Data/Cache.cs
@@ -8,9 +8,10 @@ namespace DeliveryService.Data
 {
     public class Cache : ICache
     {
-        private readonly Dictionary<object, object> _cache = new Dictionary<object, object>();
+        private readonly Dictionary<object, object> _cache = new();
         private readonly object _locker = new();
-        private DateTime _updateTime = new();
+        private readonly Dictionary<object, DateTime> _updateTime = new();
+        private const int _invalidationTime = 5;
 
         public enum CollectionType
         {
@@ -23,14 +24,18 @@ namespace DeliveryService.Data
         {
             lock (_locker)
             {
-                if ((DateTime.Now - _updateTime).TotalMinutes > 5)
+                if (_cache.ContainsKey(key) && _updateTime.ContainsKey(key))
                 {
-                    _cache.Clear();
+                    if ((DateTime.Now - _updateTime[key]).TotalMinutes > _invalidationTime)
+                    {
+                        _cache.Remove(key);
+                        _updateTime.Remove(key);
+                    }
                 }
-                if (!_cache.ContainsKey(key))
+                if (!_cache.ContainsKey(key) && !_updateTime.ContainsKey(key))
                 {
                     _cache[key] = addToCache();
-                    _updateTime = DateTime.Now;
+                    _updateTime[key] = DateTime.Now;
                 }
                 return _cache[key];
             }
