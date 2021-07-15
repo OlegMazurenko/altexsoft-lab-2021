ac9cd02ec67f40d5892a83d5ba6d66e1bbcca438
OlegMazurenko <63805434+OlegMazurenko@users.noreply.github.com>
2021-07-15 22:47:29 +0300

    Add models

---
+++ Module3/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeliveryService.Models
+{
+    public class User : BaseModel
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public AccessLevel Access { get; set; }
+
+        public enum AccessLevel
+        {
+            Admin,
+            Buyer,
+            Seller,
+            DeliveryMan
+        }
+
+        public User(string email, string password, string name, string phoneNumber, AccessLevel access)
+        {
+            Email = email;
+            Password = password;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Access = access;
+        }
+    }
+}
