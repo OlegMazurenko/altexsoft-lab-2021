a07fdcf37b8c2644f45e7991a62625c6d24ff196
OlegMazurenko <63805434+OlegMazurenko@users.noreply.github.com>
2021-08-11 15:10:43 +0300

    Add project files.

---
+++ DeliveryServiceEF.Domain/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using DeliveryServiceEF.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeliveryServiceEF.Domain.Models
+{
+    public class User : BaseModel
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public AccessLevel Access { get; set; }
+        public IList<Order> Orders { get; set; }
+    }
+}
---
+++ DeliveryServiceEF.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeliveryServiceEF.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
