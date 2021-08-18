5feacd1d02be92c1861c2ded646a1aa4a7fa06bb
OlegMazurenko <63805434+OlegMazurenko@users.noreply.github.com>
2021-08-18 15:04:06 +0300

    Add trigger

---
+++ DeliveryServiceEF/DeliveryServiceEF.Data/Migrations/20210818113428_trigger.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliveryServiceEF.Data.Migrations
+{
+    public partial class trigger : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE TRIGGER RenameCategory ON Categories AFTER INSERT AS UPDATE Categories SET Name = Name + 'category'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER RenameCategory");
+        }
+    }
+}
