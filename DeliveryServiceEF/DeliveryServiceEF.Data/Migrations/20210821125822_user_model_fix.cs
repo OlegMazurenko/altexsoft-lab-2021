43ad3121a467941f4bd7ab6bb98a52c62b0be98c
OlegMazurenko <63805434+OlegMazurenko@users.noreply.github.com>
2021-08-21 17:17:30 +0300

    Models and repositories fix

---
+++ DeliveryServiceEF/DeliveryServiceEF.Data/Migrations/20210821125822_user_model_fix.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliveryServiceEF.Data.Migrations
+{
+    public partial class user_model_fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Name");
+        }
+    }
+}
