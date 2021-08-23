1a873e89d70b8615b0885c8332f312e8f87bfc7c
OlegMazurenko <63805434+OlegMazurenko@users.noreply.github.com>
2021-08-23 22:57:13 +0300

    Add list of endpoints

--- DeliveryServiceASP/DeliveryServiceEF.Web/Controllers/CategoryController.cs
+++ DeliveryServiceASP/DeliveryServiceEF.Web/Controllers/CategoryController.cs
@@ -22,12 +22,31 @@ namespace DeliveryServiceEF.Web.Controllers
             _categoryService = categoryService;
         }
 
-        //  GET /Category
         [HttpGet]
         public IEnumerable<Category> Get()
         {
             _categoryService.AddCategory(new Category() { Name = "Category123" });
             return _categoryService.GetCategories();
         }
+
+        //  GET /Category/{id?}
+        //  POST /Category
+        //  PUT /Category/{id}
+        //  DELETE /Category/{id}
+
+        //  GET /Order/{id?}
+        //  POST /Order
+        //  PUT /Order/{id}
+        //  DELETE /Order/{id}
+
+        //  GET /Product/{id?}
+        //  POST /Product
+        //  PUT /Product/{id}
+        //  DELETE /Product/{id}
+
+        //  GET /User/{id?}
+        //  POST /User
+        //  PUT /User/{id}
+        //  DELETE /User/{id}
     }
 }
