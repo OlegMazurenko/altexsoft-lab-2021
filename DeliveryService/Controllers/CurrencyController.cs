2ae34097dc8b2c624a6473b9504db7c1fe3b4422
OlegMazurenko <63805434+OlegMazurenko@users.noreply.github.com>
2021-08-28 16:16:54 +0300

    Add currency serialization

--- DeliveryService/Controllers/CurrencyController.cs
+++ DeliveryService/Controllers/CurrencyController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using DeliveryService.Interfaces;
 using System.Globalization;
+using DeliveryService.Models;
+using System.Text.Json;
 
 namespace DeliveryService.Controllers
 {
@@ -14,12 +16,8 @@ namespace DeliveryService.Controllers
         public async Task<decimal> GetUsdRateAsync()
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetStringAsync("https://bank.gov.ua/NBUStatService/v1/statdirectory/dollar_info?json");
-            var rate = response
-                .Split(new string[] { "\"rate\":" }, StringSplitOptions.RemoveEmptyEntries)[1]
-                .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)[0];
-            decimal.TryParse(rate, NumberStyles.Any, new CultureInfo("en-US"), out var parsedRate);
-            return parsedRate;
+            var response = await httpClient.GetStringAsync("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
+            return JsonSerializer.Deserialize<List<Currency>>(response).First(x => x.CurrencyCode == "USD").Rate;
         }
     }
 }
